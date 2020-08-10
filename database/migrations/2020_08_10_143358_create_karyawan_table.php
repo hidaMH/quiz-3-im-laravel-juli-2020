71da78a5259e69825bd30caec1935c5992e7854d
hidaMH <hidamuhimmah@gmail.com>
2020-08-10 21:50:46 +0700

    Selesi Quiz 3

---
+++ database/migrations/2020_08_10_143358_create_karyawan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->varchar('nama');
+            $table->varchar('jabatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+}
